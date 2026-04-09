2467978e230d3dd0f3546ce85b6067aaa6bfe45f
priscillaCN <priscillannamani00@gmail.com>
2026-04-09 04:26:48 +0100

    Update AsciiValues.java

--- littleLoops/AsciiValues.java
+++ littleLoops/AsciiValues.java
@@ -2,11 +2,11 @@ public class AsciiValues {
 
 	public static void main(String[] args) {
 
-		int l = 65;
+		int i = 65;
 
-		while (l <= 90) {
-		System.out.println("The ASCII value of" + (char) l + "is" + l);
-		l++;
+		while (i <= 90) {
+		System.out.println("The ASCII value of" + (char) i + "is" + i);
+		i++;
 		}	
 
 	}
