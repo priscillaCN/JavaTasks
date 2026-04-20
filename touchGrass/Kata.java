3674612fa1501f60d7a6aba1339f3a7f142b4b96
unknown <priscillannamani00@gmail.com>
2026-04-20 01:26:35 +0100

    updated kata file

--- touchGrass/Kata.java
+++ touchGrass/Kata.java
@@ -4,41 +4,52 @@ public class Kata {
 
 	public static void main(String[] args) {
 
-		Scanner input = new Scanner(System.in);
+		Scanner collectInput = new Scanner(System.in);
 
-		System.out.println("Welcome To Priscilla's World of Methods. \nTask one is to find the max of two numbers.");
+		System.out.println("Welcome To Priscilla's World of Methods. \n\nTask one is to find the max of two numbers.\n");
 		
 		System.out.println("Enter the first number: ");
-		int firstNumber = input.nextInt();
+		int firstNumber = collectInput.nextInt();
 
 		System.out.println("Enter the second number: ");
-		int secondNumber = input.nextInt();
+		int secondNumber = collectInput.nextInt();
 
 		int max = findTheMax(firstNumber, secondNumber);
 		
-		System.out.println("The maximum of the two numbers is: " + max);
+		System.out.println("The maximum of the two numbers is: " + max + "\n");
 		
-		System.out.println("Task two is to tell if a number is even.");
+		System.out.println("Task two is to tell if a number is even.\n");
 
-		System.out.print("Enter any number: ");
-		int number = input.nextInt();
+		System.out.println("Enter any number: ");
+		int number = collectInput.nextInt();
 		
 		boolean answer = isEven(number);
 		
-		System.out.println("Is the number entered even? " + answer);
+		System.out.println("Is the number entered even? " + answer + "\n");
 		
-		System.out.println("Task four is to subtract two numbers and return a positive number irrespective of the order they are entered.");
+		System.out.println("Task four is to subtract two numbers and return a positive number \nirrespective of the order they are entered.\n");
 		
 		System.out.println("Enter the first number: ");
-		firstNumber = input.nextInt();
+		firstNumber = collectInput.nextInt();
 
 		System.out.println("Enter the second number: ");
-		secondNumber = input.nextInt();
+		secondNumber = collectInput.nextInt();
 		
 		int result = subtract(firstNumber, secondNumber);
 		
-		System.out.println("The asnwer is: " + result);
+		System.out.println("The asnwer is: " + result + "\n");
 		
+		System.out.println("Task five is to get the quotient of two integers\n");
+		
+		System.out.println("Enter the first number:");
+		float numberOne = collectInput.nextFloat();
+		
+		System.out.println("Enter the sceond number:");
+		float numberTwo = collectInput.nextFloat();
+		
+		float quotient = divide(numberOne, numberTwo);
+		
+		System.out.println("The answer is: " + .2fquotient + "\n");
 
 	}
 
@@ -48,7 +59,7 @@ public class Kata {
 				return firstNumber;
 			}
 			
-			else{
+			else {
 				return secondNumber;
 			}
 
@@ -62,16 +73,29 @@ public class Kata {
 			if(number % two == zero) {
 				return true;
 			}
+			else {
 			return false;
+			}
 
 		} 
 		
 		public static int subtract(int firstNumber, int secondNumber) {
-			if(firstNumber > secondNumber){
+			if(firstNumber > secondNumber) {
 				return firstNumber - secondNumber;
-			}else{
+			}
+			else {
 				return secondNumber - firstNumber;
 			}
+		}
+		
+		public static float divide(float numberOne, float numberTwo) {
+			if (numberTwo == 0) {
+				return (int) 0;
+			}
+			else {
+			return numberOne / numberTwo;
+			}
+			
 		}
 
 
