905cb70a455092bcbfd408d9413032356c2931ad
priscillaCN <priscillannamani00@gmail.com>
2026-06-09 14:43:18 +0100

    student grade system updated

--- StudentGrader/StudentGrade.java
+++ StudentGrader/StudentGrade.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Arrays;
+
 public class StudentGrade {
 
 
@@ -37,6 +39,159 @@ public class StudentGrade {
 
         return studentsSubjectsAndScores;
 
+
+    }
+
+
+    public static int [] calculateTheTotalScoreOfEveryStudent (int [][] studentsSubjectsAndScores, int numberOfStudents) {
+
+        int [] studentsTotalScores = new int [numberOfStudents];
+
+        for(int studentIndex = 0; studentIndex < studentsSubjectsAndScores.length; studentIndex++) {
+
+            int sumOfAllScoresPerStudent = 0;
+
+            for(int scoreIndex = 0; scoreIndex < studentsSubjectsAndScores[studentIndex].length; scoreIndex++) {
+
+                int subjectScore = studentsSubjectsAndScores[studentIndex][scoreIndex];
+                sumOfAllScoresPerStudent += subjectScore;
+            }
+
+            studentsTotalScores[studentIndex] = sumOfAllScoresPerStudent;
+
+            
+
+        }
+
+        return studentsTotalScores;
+
+    }
+
+
+    public static double [] calculateTheAverageScoreOfEveryStudent (int [] studentsTotalScores, int numberOfSubjects) {
+        
+        double [] studentsAverageScores = new double [studentsTotalScores.length];
+
+        for(int studentsTotalIndex = 0;  studentsTotalIndex < studentsTotalScores.length; studentsTotalIndex++) {
+            studentsAverageScores[studentsTotalIndex] = (double)studentsTotalScores[studentsTotalIndex] / numberOfSubjects;
+        }
+
+        return studentsAverageScores;
+    }
+
+
+    public static int [] calculateThePositionOfEveryStudent (int [] studentsTotalScores) {
+
+        int [] studentsPositions = new int [studentsTotalScores.length];
+
+        int [] studentsTotalScoresClone = studentsTotalScores.clone();
+        Arrays.sort(studentsTotalScoresClone);
+
+        for(int studentsTotalIndex = 0; studentsTotalIndex < studentsTotalScores.length; studentsTotalIndex++) {
+
+            for(int studentsTotalCloneIndex = studentsTotalScoresClone.length - 1; studentsTotalCloneIndex >= 0; studentsTotalCloneIndex--) {
+
+                 if(studentsTotalScores[studentsTotalIndex] == studentsTotalScoresClone[studentsTotalCloneIndex] )
+                    studentsPositions[studentsTotalIndex] = studentsTotalScoresClone.length -studentsTotalCloneIndex;
+
+            }
+
+        }
+
+        return studentsPositions;
+    }
+
+
+    public static int [] calculateTheNumberOfPassesForEverySubject (int [][] studentsSubjectsAndScores, int numberOfSubjects) {
+
+        int [] numberOfPassesForEachSubject = new int [numberOfSubjects];
+
+        for(int scoreIndex = 0; scoreIndex < studentsSubjectsAndScores[0].length; scoreIndex++) {
+            
+            int passCounter = 0;
+
+            for(int studentIndex = 0; studentIndex < studentsSubjectsAndScores.length; studentIndex++) {
+
+                if(studentsSubjectsAndScores[studentIndex][scoreIndex] >= 50) passCounter++;
+
+            }
+
+            numberOfPassesForEachSubject[scoreIndex] = passCounter;
+
+        }
+
+        return numberOfPassesForEachSubject;
+
+    }
+
+
+    public static int [] calculateTheNumberOfFailsForEverySubject (int [][] studentsSubjectsAndScores, int numberOfSubjects) {
+
+        int [] numberOfFailsForEachSubject = new int [numberOfSubjects];
+
+        for(int scoreIndex = 0; scoreIndex < studentsSubjectsAndScores[0].length; scoreIndex++) {
+            
+            int failCounter = 0;
+
+            for(int studentIndex = 0; studentIndex < studentsSubjectsAndScores.length; studentIndex++) {
+
+                if(studentsSubjectsAndScores[studentIndex][scoreIndex] < 50) failCounter++;
+
+            }
+
+            numberOfFailsForEachSubject[scoreIndex] = failCounter;
+
+        }
+
+        return numberOfFailsForEachSubject;
+
+    }
+
+
+    public static int [] getTheHighestScoreForEverySubject (int [][] studentsSubjectsAndScores, int numberOfSubjects) {
+    
+        int [] highestScoreForeachSubject = new int [numberOfSubjects];
+
+        for(int scoreIndex = 0; scoreIndex < studentsSubjectsAndScores[0].length; scoreIndex++) {
+
+            int highestScore = studentsSubjectsAndScores[0][scoreIndex];
+
+            for(int studentIndex = 0; studentIndex < studentsSubjectsAndScores.length; studentIndex++) {
+
+                if(highestScore < studentsSubjectsAndScores[studentIndex][scoreIndex])
+                    highestScore = studentsSubjectsAndScores[studentIndex][scoreIndex];
+     
+            }
+
+            highestScoreForeachSubject[scoreIndex] = highestScore;
+
+        }
+
+        return highestScoreForeachSubject;
+
+    }
+
+
+    public static int [] getTheLowestScoreForEverySubject (int [][] studentsSubjectsAndScores, int numberOfSubjects) {
+    
+        int [] lowestScoreForeachSubject = new int [numberOfSubjects];
+
+        for(int scoreIndex = 0; scoreIndex < studentsSubjectsAndScores[0].length; scoreIndex++) {
+
+            int lowestScore = studentsSubjectsAndScores[0][scoreIndex];
+
+            for(int studentIndex = 0; studentIndex < studentsSubjectsAndScores.length; studentIndex++) {
+
+                if(lowestScore > studentsSubjectsAndScores[studentIndex][scoreIndex])
+                    lowestScore = studentsSubjectsAndScores[studentIndex][scoreIndex];
+     
+            }
+
+            lowestScoreForeachSubject[scoreIndex] = lowestScore;
+
+        }
+
+        return lowestScoreForeachSubject;
 
     }
 
--- StudentGrader/StudentGradeTest.java
+++ StudentGrader/StudentGradeTest.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StudentGradeTest {
 
     @Test
-    public void testThatAllSubjectScoresForEachStudentAreInputedInA2DArray() {
+    public void testThatAllSubjectScoresForEachStudentAreStoredInA2DArray() {
 
         int numberOfStudents = 3;
         int numberOfSubjects = 4;
@@ -17,9 +19,87 @@ public class StudentGradeTest {
 
     }
 
-    @Test
-    public void testThat
 
+    @Test
+    public void testThatTheTotalScoreOfEachStudentIsCalculatedAndStoredInAnArray() {
+    
+        int numberOfStudents = 3;
+        int [][] studentsSubjectsAndScores = {{45, 67, 53, 50}, {67, 50, 99, 78}, {34, 77, 63, 60}};
+        int [] expected = {215, 294, 234};
+
+        assertArrayEquals(expected, StudentGrade.calculateTheTotalScoreOfEveryStudent(studentsSubjectsAndScores, 3));
+
+    }
+
+
+    @Test
+    public void testThatTheAverageScoreOfEachStudentIsCalculatedAndStoredInAnArray() {
+    
+        int numberOfSubjects = 4;
+        int [] studentsTotalScores = {215, 294, 234};
+        double [] expected = {53.75, 73.50, 58.50};
+
+        assertArrayEquals(expected, StudentGrade.calculateTheAverageScoreOfEveryStudent(studentsTotalScores, 4));
+
+    }
+
+
+    @Test
+    public void testThatThePositionOfEachStudentIsCalculatedAndStoredInAnArray() {
+    
+        int [] studentsTotalScores = {215, 294, 234};
+        int [] expected = {3, 1, 2};
+
+        assertArrayEquals(expected, StudentGrade.calculateThePositionOfEveryStudent(studentsTotalScores));
+
+    }
+
+    @Test
+    public void testThatTheNumberOfPassesInEachSubjectIsCalculatedAndStoredInAnArray() {
+    
+        int numberOfSubjects = 4;
+        int [][] studentsSubjectsAndScores = {{45, 67, 53, 50}, {67, 50, 99, 78}, {34, 77, 43, 60}};
+        int [] expected = {1, 3, 2, 3};
+
+        assertArrayEquals(expected, StudentGrade.calculateTheNumberOfPassesForEverySubject(studentsSubjectsAndScores, 4));
+
+    }
+
+
+    @Test
+    public void testThatTheNumberOfFailsInEachSubjectIsCalculatedAndStoredInAnArray() {
+    
+        int numberOfSubjects = 4;
+        int [][] studentsSubjectsAndScores = {{45, 67, 53, 50}, {67, 50, 99, 78}, {34, 77, 43, 60}};
+        int [] expected = {2, 0, 1, 0};
+
+        assertArrayEquals(expected, StudentGrade.calculateTheNumberOfFailsForEverySubject(studentsSubjectsAndScores, 4));
+
+    }
+
+
+    @Test
+    public void testThatTheHighestScoreForEachSubjectIsObtainedAndStoredInAnArray() {
+    
+        int numberOfSubjects = 4;
+        int [][] studentsSubjectsAndScores = {{45, 67, 53, 50}, {67, 50, 99, 78}, {34, 77, 43, 60}};
+        int [] expected = {67, 77, 99, 78};
+
+        assertArrayEquals(expected, StudentGrade.getTheHighestScoreForEverySubject(studentsSubjectsAndScores, 4));
+
+    }
+
+
+    @Test
+    public void testThatTheLowestScoreForEachSubjectIsObtainedAndStoredInAnArray() {
+    
+        int numberOfSubjects = 4;
+        int [][] studentsSubjectsAndScores = {{45, 67, 53, 50}, {67, 50, 99, 78}, {34, 77, 43, 60}};
+        int [] expected = {34, 50, 43, 50};
+
+        assertArrayEquals(expected, StudentGrade.getTheLowestScoreForEverySubject(studentsSubjectsAndScores, 4));
+
+    }
 
 
 
